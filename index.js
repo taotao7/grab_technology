7af6cfef3e97ce7816e0ae987c2fea5fe046333d
taotao7 <moca_tao7@foxmail.com>
2021-02-24 17:32:23 +0800

    💨💨💨差最后的统计

--- index.js
+++ index.js
@@ -1,74 +1,108 @@
-const request = require("request");
+const axios = require("axios");
 const xlsx = require("node-xlsx");
+let qs = require("qs");
 const HTMLParser = require("node-html-parser");
 const fs = require("fs");
 const monment = require("moment");
-const { SSL_OP_EPHEMERAL_RSA } = require("constants");
-const { resolve } = require("path");
 monment.locale("zh-cn");
 
-//定义休眠
-const sleep = (time) => {
-  return new Promise((resolve) => setTimeout(resolve, time));
-};
-
-//查询url
+//url
 const url = "http://202.61.88.188/xmgk/Person/rList.aspx";
 
-let allHumanData;
+//所有人的网址
+let allHumanData = [
+  {
+    name: "sheet",
+    data: [["姓名", "地址"]],
+  },
+];
 
-//读取excel
-const workSheetsFromBuffer = xlsx.parse("./assets/names.xlsx");
-workSheetsFromBuffer[0].data.forEach((element) => {
-  //定义参数
-  const options = {
-    url: url,
-    headers: {
-      Connection: "keep-alive",
-      "Accept-Encoding": "gzip, deflate",
-      Accept:
-        "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
-      "User-Agent":
-        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74",
-      Host: "202.61.88.188",
-      Origin: "http://202.61.88.188",
-      Referer: "http://202.61.88.188/xmgk/Person/rList.aspx",
-    },
-    form: {
-      __VIEWSTATE:
-        "/wEPDwUKLTk0OTA3NDI0OGRktfRWnDBOmG3vQzZKy/XGkQBYTf6y/E1Q3cPcXzK5DBs=",
-      __VIEWSTATEGENERATOR: "BF7D64B3",
-      __EVENTVALIDATION:
-        "/wEdAAIuDpyLtyFgJS1TWechhO4W+d3fBy3LDDz0lsNt4u+CuEELtJQu+cdOas7KA4kYTvVsitsWzJFprZmaicfjvdlH",
-      mc: element[0],
-      rybc: null,
-      zsbh: null,
-      sfzh: null,
-      qymc: "中科标禾工程项目管理有限公司",
-      ctl00$MainContent$Button1: "搜索",
-    },
-  };
-  //防止服务器被抓爆
-  sleep(5000);
+let allDetails = [
+  {
+    name: "sheet",
+    data: [["名字", "在建项目数量", "链接"]],
+  },
+];
 
-  //获得数据
-  request.post(options, (error, response, body) => {
-    console.log("当前时间为:", monment().format("YYYY MMMM Do"));
-    console.log("错误为:", error);
-    console.log("状态码:", response && response.statusCode);
-    console.log("输出到为:");
-    let result = HTMLParser.parse(body);
-    console.log(
-      element[0] + "  " + result.querySelectorAll("a")[12].getAttribute("href")
-    );
-    //   fs.writeFile("data.json", body, (err) => {
-    //     if (err) {
-    //       return console.error(err);
-    //     }
-    //     console.log("成功");
-    //   });
-  });
-});
+// //读取excel
+// const workSheetsFromBuffer = xlsx.parse("./assets/names.xlsx");
+// //索引
+// let index = workSheetsFromBuffer[0].data.length;
+// let currentIndex = 0;
+
+// const outPutHuman = () => {
+//   if (currentIndex < index) {
+//     //config
+//     let config = {
+//       url: url,
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "application/x-www-form-urlencoded",
+//         "Accept-Encoding": "gzip, deflate",
+//         Accept:
+//           "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+//         "Accept-Language": "zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6",
+//         "User-Agent":
+//           "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/88.0.4324.182 Safari/537.36 Edg/88.0.705.74",
+//         Host: "202.61.88.188",
+//         Origin: "http://202.61.88.188",
+//         Referer: "http://202.61.88.188/xmgk/Person/rList.aspx",
+//       },
+//       data: qs.stringify({
+//         __VIEWSTATE:
+//           "/wEPDwUKLTk0OTA3NDI0OGRktfRWnDBOmG3vQzZKy/XGkQBYTf6y/E1Q3cPcXzK5DBs=",
+//         __VIEWSTATEGENERATOR: "BF7D64B3",
+//         __EVENTVALIDATION:
+//           "/wEdAAIuDpyLtyFgJS1TWechhO4W+d3fBy3LDDz0lsNt4u+CuEELtJQu+cdOas7KA4kYTvVsitsWzJFprZmaicfjvdlH",
+//         mc: workSheetsFromBuffer[0].data[currentIndex][0],
+//         qymc: "中科标禾工程项目管理有限公司",
+//         ctl00$MainContent$Button1: "搜索",
+//       }),
+//     };
+//     axios(config)
+//       .then((response) => {
+//         console.log("当前时间为:", monment().format("YYYY MMMM Do"));
+//         console.log("状态码:", response.status);
+//         console.log("输出为:");
+//         let result = HTMLParser.parse(response.data);
+//         //抓取信息
+//         console.log(
+//           workSheetsFromBuffer[0].data[currentIndex][0] +
+//             ": http://202.61.88.188/xmgk/Person/" +
+//             result.querySelectorAll("a")[12].getAttribute("href")
+//         );
+//         console.log(workSheetsFromBuffer[0].data[currentIndex][0] + "抓取成功");
+
+//         allHumanData[0].data.push([
+//           workSheetsFromBuffer[0].data[currentIndex][0],
+
+//           "http://202.61.88.188/xmgk/Person/" +
+//             result.querySelectorAll("a")[12].getAttribute("href"),
+//         ]);
+
+//         //增加索引
+//         currentIndex += 1;
+//       })
+//       .catch((error) => {
+//         console.log("错误为:", error);
+//       });
+//   } else {
+//     console.log("完成抓取");
+//     //buffer
+//     let buffer = xlsx.build(allHumanData);
+
+//     fs.writeFile("data.xlsx", buffer, (err) => {
+//       if (err) {
+//         return console.error(err);
+//       }
+//     });
+//     console.log("完成抓取稍等开始解析");
+//     clearInterval(intervel);
+//   }
+// };
+
+// //开始运行
+// let intervel = setInterval(outPutHuman, 3000);
 
 //TODO toExcel
 
@@ -78,8 +112,25 @@ workSheetsFromBuffer[0].data.forEach((element) => {
 //     data: [["店名", "地址", "区域", "最低消费", "评价留言", "评分"]],
 //   },
 // ];
-// let allData = fs.readFileSync("data.json");
-// let formatData = JSON.parse(allData);
+let allData = fs.readFileSync("data.xlsx");
+let formatData = xlsx.parse(allData);
+//最终数据buffer
+let detailBuffer;
+formatData[0].data.forEach((value) => {
+  allDetails[0].data.push([value[0], "链接", value[1]]);
+});
+
+detailBuffer = xlsx.build(allDetails);
+
+console.log(allDetails);
+fs.writeFile("final.xlsx", detailBuffer, (err) => {
+  if (err) {
+    console.log("遇到错误:");
+    return console.error(err);
+  }
+});
+
+// console.log("全部完毕可以查看!!");
 
 // formatData.data.searchResult.forEach((element) => {
 //   excelData[0].data.push([
